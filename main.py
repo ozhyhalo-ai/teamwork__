58085d4b613a7806ac3caa5e6017f6cd36f1988a
Bohdan Boyko <bogdanboiko07@gmail.com>
2026-03-10 09:55:56 +0200

    NSD

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from utils import factorial
 
 number = 5
 print(f'Факторіал числа {number} дорівнює {factorial(number)}')
+from utils import factorial, calculate_gcd
+num1 = 48
+num2 = 18
--- utils.py
+++ utils.py
@@ -9,3 +9,7 @@ def factorial(n):
     for i in range(2, n + 1):
         result *= i
     return result
+def calculate_gcd(a, b):
+while b!=0:
+    a, b = b, a%b
+return a
